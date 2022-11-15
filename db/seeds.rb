89592276e1289a4501354dd861b6ac21f45c058c
Tom Greenwood <tom.t.greenwood@gmail.com>
2022-11-15 19:51:20 +0100

    barely finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+articles = Article.create([{ name: Faker::Book.title}], [{content: Fakker::}])
